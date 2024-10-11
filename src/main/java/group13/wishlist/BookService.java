dcf3c83b35bd1505a8c105c63113215961470d6f
ashley <132930723+xeahh@users.noreply.github.com>
2024-10-11 13:58:14 -0700

    removed @AutoWired and added BookRepository constructors

--- src/main/java/group13/wishlist/BookService.java
+++ src/main/java/group13/wishlist/BookService.java
@@ -1,14 +1,16 @@
 package group13.wishlist;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import java.util.List;
 
 @Service
 public class BookService {
 
-  @Autowired
-  private BookRepository bookRepository;
+  private final BookRepository bookRepository;
+
+  public BookService(BookRepository bookRepository) {
+    this.bookRepository = bookRepository;
+  }
 
   // Fetch all books
   public List<Book> getAllBooks() {
