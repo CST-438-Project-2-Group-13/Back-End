6289067d2593b0f5a5f7c312a01af5b59a01432e
tr3nt-tayl0r <tretaylor@csumb.edu>
2024-10-12 13:26:17 -0700

    added react sandbox url to WebConfig.java and added allowedHeaders

--- src/main/java/group13/wishlist/config/WebConfig.java
+++ src/main/java/group13/wishlist/config/WebConfig.java
@@ -11,6 +11,9 @@ public class WebConfig implements WebMvcConfigurer {
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
                 .allowedOrigins("http://localhost:3000","https://codesandbox.io/")
-                .allowedMethods("GET", "POST", "PUT", "DELETE");
+                .allowedMethods("GET", "POST", "PUT", "DELETE")
+                .allowedHeaders("*")  
+                .allowCredentials(true);
     }
+
 }
