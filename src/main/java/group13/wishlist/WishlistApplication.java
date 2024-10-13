49b8ebe119a3ada0fc8eb4464b32c1ff5403379b
ashley <132930723+xeahh@users.noreply.github.com>
2024-10-13 11:09:34 -0700

    added cors mapping

--- src/main/java/group13/wishlist/WishlistApplication.java
+++ src/main/java/group13/wishlist/WishlistApplication.java
@@ -1,11 +1,23 @@
 package group13.wishlist;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 public class WishlistApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(WishlistApplication.class, args);
+	}
+	@Bean
+	public WebMvcConfigurer corsConfigurer() {
+		return new WebMvcConfigurer() {
+			@Override
+			public void addCorsMappings(CorsRegistry registry) {
+				registry.addMapping("/**").allowedOrigins("https://plotpicks-b82378f80d9c.herokuapp.com/");
+			}
+		};
 	}
 }
