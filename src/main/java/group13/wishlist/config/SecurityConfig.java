a1d004836f02db25774d38e82b3c2c0400c28088
ashley <132930723+xeahh@users.noreply.github.com>
2024-10-15 13:25:48 -0700

    cors

--- src/main/java/group13/wishlist/config/SecurityConfig.java
+++ src/main/java/group13/wishlist/config/SecurityConfig.java
@@ -2,7 +2,6 @@ package group13.wishlist.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.annotation.authentication.configuration.GlobalAuthenticationConfigurerAdapter;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
@@ -14,9 +13,9 @@ import static org.springframework.security.config.Customizer.withDefaults;
 public class SecurityConfig {
 
     @Bean
-    public SecurityFilterChain securityFilterChain(HttpSecurity http, GlobalAuthenticationConfigurerAdapter enableGlobalAuthenticationAutowiredConfigurer) throws Exception {
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
-            .cors(AbstractHttpConfigurer::disable)
+//            .cors(withDefaults())
             .authorizeHttpRequests(authorize -> authorize
                     .requestMatchers("/**").permitAll()
             )
