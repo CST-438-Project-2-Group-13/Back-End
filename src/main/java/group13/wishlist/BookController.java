2a4918cc5d3b7fb41d679074644230b0327ece87
Trent Taylor <111828304+tr3nt-tayl0r@users.noreply.github.com>
2024-10-15 19:15:18 -0700

    removed /config and added controller-level @CrossOrigin annotation

--- src/main/java/group13/wishlist/BookController.java
+++ src/main/java/group13/wishlist/BookController.java
@@ -1,7 +1,12 @@
 package group13.wishlist;
 
+import com.google.gson.JsonArray;
+import com.google.gson.JsonObject;
+import com.google.gson.JsonParser;
+import java.util.ArrayList;
 import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -11,13 +16,9 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
-import com.google.gson.JsonArray;
-import com.google.gson.JsonObject;
-import com.google.gson.JsonParser;
-
-import java.util.ArrayList;
 
 @RestController
+@CrossOrigin
 public class BookController {
 
   @Autowired
@@ -62,7 +63,7 @@ public class BookController {
     return books;
   }
 
-
+  
   @GetMapping("/api/books")
   public List<Book> searchBooks(
       @RequestParam(required = false) String title,
