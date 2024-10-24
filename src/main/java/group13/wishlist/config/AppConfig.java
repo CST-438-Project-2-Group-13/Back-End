2dba11e19336aa8b4c20d7b612883e71628fd763
ashley <132930723+xeahh@users.noreply.github.com>
2024-10-24 13:04:09 -0700

    added JwtAuthenticationFilter in AppConfig.java

--- src/main/java/group13/wishlist/config/AppConfig.java
+++ src/main/java/group13/wishlist/config/AppConfig.java
@@ -1,10 +1,13 @@
 package group13.wishlist.config;
 
+import group13.wishlist.JwtAuthenticationFilter;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
@@ -13,6 +16,7 @@ import org.springframework.security.config.annotation.web.configurers.AbstractHt
 
 @Configuration
 @EnableWebSecurity
+@EnableGlobalMethodSecurity(prePostEnabled = true)
 public class AppConfig implements WebMvcConfigurer {
 
     @Override
@@ -29,10 +33,17 @@ public class AppConfig implements WebMvcConfigurer {
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
                 .authorizeHttpRequests(authorize -> authorize
+                        .requestMatchers("/api/admin/**").hasRole("ADMIN")
                         .requestMatchers("/**").permitAll()
                 )
                 .cors(withDefaults())
                 .csrf(AbstractHttpConfigurer::disable);  // Disable CSRF protection
+        http.addFilterBefore(jwtAuthenticationFilter(), UsernamePasswordAuthenticationFilter.class);
         return http.build();
+    }
+
+    @Bean
+    public JwtAuthenticationFilter jwtAuthenticationFilter() {
+        return new JwtAuthenticationFilter();
     }
 }
